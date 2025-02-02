a1b2d18d202f21be241c2068c0c9ca3f1d860b9d
Joe-encodes <josephadqmu2019@gmail.com>
2025-02-02 22:27:25 +0100

    Update FASTAPI code for readability

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-from fastapi import FastAPI
-from datetime import datetime, timezone
+from fastapi import FastAPI # Import a web framework that's used to build APIs
+from datetime import datetime, timezone 
 
-app = FastAPI()
+app = FastAPI() # Create an instance of the FastAPI class
 
-@app.get("/")
+@app.get("/") # Define a route for the root of the API
 def get_info():
     return {
-        "email": "your-email@example.com",  # Replace with your registered HNG email
-        "current_datetime": datetime.now(timezone.utc).isoformat(),
-        "github_url": "https://github.com/yourusername/your-repo"  # Replace with your actual GitHub repo URL
+        "email": "joseph.adamu.it@gmail.com",  # Registered HNG email
+        "current_datetime": datetime.now(timezone.utc).isoformat(), # Display current datetime in UTC
+        "github_url": "https://github.com/Joe-encodes/fastapi-project"
     }
 
-if __name__ == "__main__":
-    import uvicorn
+if __name__ == "__main__": # Check if the script is being run directly
+    import uvicorn # Import a Python Web Server to handle requests, it communicates with the API
     uvicorn.run(app, host="0.0.0.0", port=8000)
 
